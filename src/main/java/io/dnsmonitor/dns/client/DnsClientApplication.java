606dad11d413871b14cbc2008f48f2a5ad31bc4b
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2020-04-09 18:01:01 +0100

    Initial commit

---
+++ src/main/java/io/dnsmonitor/dns/client/DnsClientApplication.java
@@ -0,0 +1,13 @@
+package io.dnsmonitor.dns.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dnsmonitor/dns/client/DnsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dnsmonitor.dns.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
