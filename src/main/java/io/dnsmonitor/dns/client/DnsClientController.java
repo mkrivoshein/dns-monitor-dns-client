d4c1d02989a4aad7ddcfca9406713ab8b7e3bb07
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2023-01-10 01:47:09 +0000

    more changes for Spring Boot 3.x upgrade

--- src/main/java/io/dnsmonitor/dns/client/DnsClientController.java
+++ src/main/java/io/dnsmonitor/dns/client/DnsClientController.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
+import jakarta.validation.ConstraintViolationException;
 
 @RestController
 @Validated
--- src/main/java/io/dnsmonitor/dns/client/validators/DnsRecordType.java
+++ src/main/java/io/dnsmonitor/dns/client/validators/DnsRecordType.java
@@ -2,10 +2,10 @@ package io.dnsmonitor.dns.client.validators;
 
 import io.dnsmonitor.dns.client.dnsjava.DnsJavaAdapter;
 
-import javax.validation.Constraint;
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
-import javax.validation.Payload;
+import jakarta.validation.Constraint;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.Payload;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
