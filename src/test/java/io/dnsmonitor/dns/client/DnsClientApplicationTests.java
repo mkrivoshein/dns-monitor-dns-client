636adf11fc1af512a00f6863f3c0fa0cd5412a7b
Mikhail Krivoshein <mikhail.krivoshein@gmail.com>
2023-01-10 02:29:05 +0000

    Spring Boot 3 doesn't autoconfigure OpenTelemetry tracer

--- src/test/java/io/dnsmonitor/dns/client/DnsClientApplicationTests.java
+++ src/test/java/io/dnsmonitor/dns/client/DnsClientApplicationTests.java
@@ -2,8 +2,11 @@ package io.dnsmonitor.dns.client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.sleuth.autoconfig.otel.OtelAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {OtelAutoConfiguration.class})
 class DnsClientApplicationTests {
 
 	@Test
